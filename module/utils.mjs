f1be712895f9eacb3c61f195f1e5bebbaa57fc42
Zero12 <ranses1212@gmail.com>
2025-05-25 13:33:42 -0600

    Update utils.mjs

--- module/utils.mjs
+++ module/utils.mjs
@@ -1,6 +1,9 @@
-export function debug_log(message) {
-  console.log("[debug]", message);
-  if (window.debug_log) {
-    window.debug_log(message);
-  }
+export function debug_log(msg) {
+  console.log("[DEBUG] " + msg);
+  try {
+    const p = document.createElement("p");
+    p.textContent = "[DEBUG] " + msg;
+    p.style.color = "lime";
+    document.body.appendChild(p);
+  } catch (_) {}
 }
